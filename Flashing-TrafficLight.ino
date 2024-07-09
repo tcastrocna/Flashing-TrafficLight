1af12806e2ab0b12e0b380ca24067db7d4cf03fb
Tiago Castro <tiagocastrodacunha3005@gmail.com>
2024-07-09 02:15:46 -0300

    📝 Criado

--- Flashing-TrafficLight.ino
+++ Flashing-TrafficLight.ino
@@ -1,11 +1,3 @@
-## Semáforo com pisca Alerta
-Em algumas cidades os semáforos entram em alerta durante a madrugada, ou seja, apenas os leds na cor amarela ficam piscando fazendo com que o motorista preste apenas atenção ao cruzar, esse método é utilizado em cidades com pouco ou quase sem nenhum trânsito na madrugada, isso evita o motorista ficar parado no sinal e ser assaltado por exemplo. E este é um projeto simples para simular esta situação seja para fazer apenas por diversão ou educação, consite em quando o botão físico for desativado o semáforo entra em modo alerta, ao botão ser ativado o sinal funcionará normalmente.
-
-### Esquema de montagem
-![Esquema de montagem](Esquema.png)
-
-### Código Fonte
-```cpp
 #include <Arduino.h>
 
 // Define as portas dos LEDs no ESP32
@@ -112,5 +104,3 @@ void mudarModo() {
     ultimaTrocaModo = tempoAtual;
   }
 }
-
-```
